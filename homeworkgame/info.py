00d0bd698071c0750cdaee4876b3ec4c7c4eb77a
gakles <ghorchner@visiometrix.com>
2023-12-02 19:33:32 -0800

    i need to turn desk into an object :(

--- homeworkgame/info.py
+++ homeworkgame/info.py
@@ -6,12 +6,10 @@ def draw_static_info(screen, window_width, window_height, info_zone_height, cale
     pygame.draw.rect(screen, (192, 192, 192), (0, 0, window_width, info_zone_height))  # Light gray info zone
 
     # Draw calendar subzone in the left 1/3 of the info zone
-    calendar_rect = calendar_image.get_rect(left=0, top=0)
-    screen.blit(calendar_image, calendar_rect)
+    calendar_image.draw(screen)
 
     # Draw static-image subzone in the center vertical 1/3 of the info zone
-    static_image_rect = static_image.get_rect(center=(window_width // 2, info_zone_height // 2))
-    screen.blit(static_image, static_image_rect)
+    static_image.draw(screen)
 
     # Draw daily schedule subzone in the right 1/3 of the info zone
     pygame.draw.rect(screen, (192, 192, 192), (2 * window_width // 3, 0, schedule_width, info_zone_height))  # Light gray schedule zone
@@ -40,38 +38,43 @@ def draw_static_info(screen, window_width, window_height, info_zone_height, cale
             screen.blit(text, text_rect)
 
     # Draw pillow image to fill the empty space between the 3 boxes on the 4th row and the bottom of the info zone
-    pillow_rect = pillow_image.get_rect(left=2 * window_width // 3 + 3 * box_width - 4, top=info_zone_height - pillow_image.get_height() - 2)
-    screen.blit(pillow_image, pillow_rect)
+    pillow_image.draw(screen)
 
-def draw_dynamic_info(screen, window_width, window_height, info_zone_height, time_of_day):
-    # Convert minutes to hours and minutes
-    hours = time_of_day // 60
-    minutes = time_of_day % 60
-    
-    # Convert to 12-hour time format
-    am_pm = "AM" if hours < 12 else "PM"
-    hours = hours % 12 or 12  # Make 0 or 12 display as 12
-    
-    # Create a string in the format "hh:mm AM/PM"
-    time_string = f"{hours:02d}:{minutes:02d}"
-    ampm_string = f"{am_pm}"
-    font = pygame.font.Font(None, 30)
-    num_text = font.render(time_string, True, (200, 100, 200))
-    day_cycle_text = font.render(ampm_string, True, (200, 100, 200))
-    
-    # Fixed width for the rectangle (1/9 of the total screen width)
-    rect_width = window_width // 14
-    rect_height = 30  # Adjust the height as needed
-    
-    # Get the bottom right corner of the existing text_rect
-    num_text_rect = num_text.get_rect(topleft=((2*window_width/3) - 15*rect_width/16, info_zone_height-7*rect_height/8))
-    day_cycle_text_rect = num_text.get_rect(topleft=((2*window_width/3) - .35*rect_width, info_zone_height-7*rect_height/8))
+def draw_dynamic_info(type, screen, window_width, window_height, info_zone_height, time_of_day, button_image):
+    if type == "timeupdate":
+        # Convert minutes to hours and minutes
+        hours = time_of_day // 60
+        minutes = time_of_day % 60
+        
+        # Convert to 12-hour time format
+        am_pm = "AM" if hours < 12 else "PM"
+        hours = hours % 12 or 12  # Make 0 or 12 display as 12
+        
+        # Create a string in the format "hh:mm AM/PM"
+        time_string = f"{hours:02d}:{minutes:02d}"
+        ampm_string = f"{am_pm}"
+        font = pygame.font.Font(None, 30)
+        num_text = font.render(time_string, True, (200, 100, 200))
+        day_cycle_text = font.render(ampm_string, True, (200, 100, 200))
+        
+        # Fixed width for the rectangle (1/9 of the total screen width)
+        rect_width = window_width // 14
+        rect_height = 30  # Adjust the height as needed
+        
+        # Get the bottom right corner of the existing text_rect
+        num_text_rect = num_text.get_rect(topleft=((2*window_width/3) - 15*rect_width/16, info_zone_height-7*rect_height/8))
+        day_cycle_text_rect = num_text.get_rect(topleft=((2*window_width/3) - .35*rect_width, info_zone_height-7*rect_height/8))
 
-    # Adjust the position to place the rectangle at the bottom right corner
-    rect_position = (2*window_width/3 - rect_width, info_zone_height - rect_height)
-    
-    # Draw the rectangle independently
-    pygame.draw.rect(screen, (192, 192, 192), (*rect_position, rect_width, rect_height))
-    
-    screen.blit(num_text, num_text_rect)
-    screen.blit(day_cycle_text, day_cycle_text_rect)
+        # Adjust the position to place the rectangle at the bottom right corner
+        rect_position = (2*window_width/3 - rect_width, info_zone_height - rect_height)
+        
+        # Draw the rectangle independently
+        pygame.draw.rect(screen, (192, 192, 192), (*rect_position, rect_width, rect_height))
+        
+        screen.blit(num_text, num_text_rect)
+        screen.blit(day_cycle_text, day_cycle_text_rect)
+    elif type == "drawbutton":
+        button_rect = pygame.Rect(round(button_image.position[0]), round(button_image.position[1]), button_image.width, button_image.height)
+        pygame.draw.rect(screen, (192, 192, 192), button_rect)
+
+        button_image.draw(screen)
