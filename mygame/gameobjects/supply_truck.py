36941b3234b6bf551b275bebe3ed75eded45c4d6
gakles <ghorchner@visiometrix.com>
2023-12-23 21:05:31 -0700

    changes to ural

--- mygame/gameobjects/supply_truck.py
+++ mygame/gameobjects/supply_truck.py
@@ -4,6 +4,7 @@ from gameobjects.vehicle import Vehicle
 class Supply_Truck(Vehicle):
     def __init__(self, stats):
         super().__init__(stats)
+        self.currentsupply = 0 
 
     def honk_horn(self):
         print("Honk! I'm a " + self.stats["class"])
--- mygame/gameobjects/vehicle.py
+++ mygame/gameobjects/vehicle.py
@@ -1,4 +1,5 @@
 class Vehicle:
     def __init__(self, stats):
         self.stats = stats
+        self.currentfuel = 10 #starter amound
             
