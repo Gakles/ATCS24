5ec0600bb62a65d2aee32942759bb39834131042
gakles <ghorchner@visiometrix.com>
2023-12-25 21:14:34 -0700

    :( ts isnt gonna be fun. going to pivot to arena style

--- mygame/gameobjects/tank.py
+++ mygame/gameobjects/tank.py
@@ -4,3 +4,6 @@ from gameobjects.treaded_vehicle import Treaded_Vehicle
 class Tank(Treaded_Vehicle):
     def __init__(self,stats):
         super().__init__(stats)
+        self.turret_rotation = 0
+        
+    
